8bf4a5b8f2c4572cd79b1ecab6c568200e6ca523
Daniel-Ola <danorelanre@gmail.com>
2019-01-15 13:48:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>All work links</title>
+</head>
+<body>
+	<ol>
+		<li><a href="../captain" target="_blank">Captain</a></li>
+		<li><a href="../cbt" target="_blank">CBT</a></li>
+		<li><a href="../clearance" target="_blank">Clearance</a></li>
+		<li><a href="../implementation" target="_blank">Implementation</a></li>
+		<li><a href="../intellectual'17" target="_blank">Intellectual'17</a></li>
+		<li><a href="../misc" target="_blank">Miscellaneous</a></li>
+		<li><a href="../moscow" target="_blank">Moscow</a></li>
+		<li><a href="../myprofile" target="_blank">My Profile</a></li>
+		<li><a href="../myprofile/advanced" target="_blank">My Profile (Advanced)</a></li>
+		<li><a href="../onenacoss" target="_blank">Onenacoss</a></li>
+		<li><a href="../digix" target="_blank">Digix</a></li>
+		<li><a href="../runningboards" target="_blank">Runningboards</a></li>
+		<li><a href="../perez" target="_blank">Perez</a></li>
+		<li><a href="../pst" target="_blank">Pst</a></li>
+		<li><a href="../sdc" target="_blank">Student Disciplinary Committee (SDC)</a></li>
+		<li><a href="../stevot" target="_blank">Stevot</a></li>
+		<li><a href="../temp" target="_blank">Temoplates</a></li>
+	</ol>
+
+</body>
+</html>
